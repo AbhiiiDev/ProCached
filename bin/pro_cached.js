1af784657380cceecbd80df94614bfd4db05f3de
Abhishek Verma <abhishekverma@192.168.1.4>
2025-10-28 00:59:12 +0530

    Initialising server

--- bin/pro_cached.js
+++ bin/pro_cached.js
@@ -2,18 +2,11 @@ import { Command } from 'commander';
 const program = new Command();
 
 program
-  .name('string-util')
-  .description('CLI to some JavaScript string utilities')
-  .version('0.8.0');
+  .name('ProCached')
+  .description('A simple caching proxy server CLI tool')
 
-program.command('split')
-  .description('Split a string into substrings and display as an array')
-  .argument('<string>', 'string to split')
-  .option('--first', 'display just the first substring')
-  .option('-s, --separator <char>', 'separator character', ',')
-  .action((str, options) => {
-    const limit = options.first ? 1 : undefined;
-    console.log(str.split(options.separator, limit));
-  });
+program.option('--port <number>','Port to run the proxy server').option('--origin <url>', 'Origin server URL to forward requests')
+  .option('--clear-cache', 'Clear the cache')
+  .parse(process.argv);
 
-program.parse();
+const opts=program.opts();
---
+++ src/server.js
@@ -0,0 +1,6 @@
+import express from 'express'
+import chalk from 'chalk'
+export const startServer = (port,origin)=> {
+const app=express()
+app.use()
+}
